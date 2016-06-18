e626cd8d594f22b50c62cf47470f486552d68713
SelcukBilgen <selcukbilgn@gmail.com>
2016-06-18 15:14:17 +0300

    Course 2 - Week 3 - Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 1. Function(makeCacheMatrix) simply caches the matrix
+## that was calculated previously.
 
-## Write a short comment describing this function
+## 2. Function(CacheSolve) calculates the reverse of a square matrix
+## after it checks the matrix is cached or not in order to avoid calculating
+## the reverse of the same matrix again.
 
+## This function creates a list that holds the matrix and its necessary
+## methods.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setReverse <- function(solve) m <<- solve
+  getReverse <- function() m
+  list(set = set, get = get,
+       setReverse = setReverse,
+       getReverse = getReverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function checks if the matrix is cached or not. If so, it simply
+## return the reverse of the matrix using the getReverse method of the matrix.
+## If the matrix is not cached, then it calculates the reverse of the matrix
+## using solve function.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getReverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setReverse(m)
+  m
+  
 }
